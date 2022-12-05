693ab2f2715478107518b28f32d3846342b40f23
PANKAJ <PANKAJ@DESKTOP-LV1N0V2>
2022-12-05 15:48:19 +0530

    [nayana]:demo class create

---
+++ src/Sample1/Demo.java
@@ -0,0 +1,11 @@
+package Sample1;
+
+public class Demo 
+{
+public static void main(String[] args) {
+	System.out.println("hi");
+}
+}
+//UN : nayana_deshmukh
+//PWD : deshmukh1997              // token : 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
+//URL : https://github.com/nayana-deshmukh/Github_Project.git
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author PANKAJ
+ *
+ */
+module Github_Project {
+}
